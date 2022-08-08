ee12388c466d0b36bc14cf848a92712648106b7f
refszin <refwire@gmail.com>
2022-08-08 15:39:02 -0300

    refactor: v1

--- src/components/MainHistory/MainHistory.js
+++ src/components/MainHistory/MainHistory.js
@@ -5,7 +5,6 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import UserContext from "../../UserContext";
 import { useContext, useEffect, useState} from "react";
-import moment from "moment";
 export default function MainHabit(){
     const {ParticlesJss,localmenteLogado,historicData} = useContext(UserContext);
     const [calDate, setCalDate] = useState(new Date());
@@ -28,16 +27,6 @@ export default function MainHabit(){
                 value={calDate}
                 locale={'pt-br'}
                 tileClassName={({ date }) => {
-                for (let i = 0; i < historicData.length; i++) {
-                    let k = 0;
-                    if (historicData[i].day === moment(date).format("DD/MM/YYYY")) {
-                        for (let j = 0; j < historicData[i].habits.length; j++) {
-                            if (historicData[i].habits[j].done === true) {
-                                k++;
-                            }
-                        }
-                    }
-                }
             }}/>
             </BoxCalendar>
             <ParticlesJss/>
